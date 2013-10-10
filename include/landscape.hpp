b298cd176d8cbee85ba52356a78f4ee640f334ae
Michal Kawalec <michal@bazzle.me>
2013-10-10 13:18:43 +0100

    added a basic documentation to the landscape files

--- include/landscape.hpp
+++ include/landscape.hpp
@@ -1,6 +1,10 @@
 #ifndef PUMAS_landscape_h
 #define PUMAS_landscape_h
 
+/** The basic stucture holding info about the landscape
+ *  tile. If the element is_land, then pumas and hares can migrate
+ *  to it and breed on it.
+ */
 struct landscape {
     double hare_density, puma_density;
     bool is_land;
