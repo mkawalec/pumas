e17d6b0f4401d241bc7c4c874d92bec63919dbd0
Michal Kawalec <michal@bazzle.me>
2013-10-22 21:59:26 +0100

    retabbed and indented

--- src/solver.cpp
+++ src/solver.cpp
@@ -4,32 +4,32 @@
 
 int main(int argc, char *argv[])
 {
-	int total_timesteps = 100;
-	double dt = 0.1;
+    int total_timesteps = 100;
+    double dt = 0.1;
     std::ofstream hare_densities, puma_densities;
     size_t size_x, size_y;
-	
+
     std::ifstream input("input.txt");
     input >> size_x >> size_y;
     bool *land_map = new bool[size_x*size_y];
 
-	for (size_t j = 0; j < size_y; ++j) {
+    for (size_t j = 0; j < size_y; ++j) {
         for (size_t i = 0; i < size_x; ++i) {
-	        input >> land_map[i + size_x * j];
-		}
-	}
-	input.close();
-	
-	PUMA::Simulator simulation(size_x, size_y, land_map, dt);
-	delete[] land_map;
+            input >> land_map[i + size_x * j];
+        }
+    }
+    input.close();
 
-	for (int i = 0; i < total_timesteps; ++i) {
-	    simulation.apply_step();
+    PUMA::Simulator simulation(size_x, size_y, land_map, dt);
+    delete[] land_map;
 
-	    hare_densities.open("hare_hensities_" + std::to_string(i) + ".dat");
-	    puma_densities.open("puma_densities_" + std::to_string(i) + ".dat");
-	    simulation.serialize(&hare_densities, &puma_densities);
-	}
+    for (int i = 0; i < total_timesteps; ++i) {
+        simulation.apply_step();
+
+        hare_densities.open("hare_hensities_" + std::to_string(i) + ".dat");
+        puma_densities.open("puma_densities_" + std::to_string(i) + ".dat");
+        simulation.serialize(&hare_densities, &puma_densities);
+    }
 
     return 0;
 }
