4e14ecbb52ee45cb478f549fbc8282b3bd835e3e
Michal Kawalec <michal@bazzle.me>
2013-11-04 22:36:38 +0000

    commented helpers class

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -2,8 +2,8 @@
 #define PUMA_helpers_h
 
 namespace PUMA {
-    /** The basic stucture holding info about the landscape
-     *  tile. If the element is_land, then pumas and hares can migrate
+    /** The basic stucture holding information about a landscape
+     *  tile. If an element is_land, then pumas and hares can migrate
      *  to it and breed on it.
      */
     struct landscape {
@@ -15,12 +15,20 @@ namespace PUMA {
         double hares, pumas;
     };
 
+    /** Structure containing RGB colors
+     */
     struct rgb {
         double r, g, b;
     };
 
+    /** A function template accepting any
+     *  parameter and doing nothing to it. Useful
+     *  for explicit marking of a parameter as unused,
+     *  both for helping a programmer and muting compiler
+     *  warnings
+     */
     template <typename T>
-        void ignore(T &&) { }
+    void ignore(T &&) { }
 
 }
 #endif
